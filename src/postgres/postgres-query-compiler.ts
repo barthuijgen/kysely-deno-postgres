4e74b53c0c7b285dd5fd487c8520d7d76027c241
Bart Huijgen <bhuijgen@bol.com>
2022-01-12 20:56:15 +0100

    clean: re-use parts from the original dialects

--- src/postgres/postgres-query-compiler.ts
+++ src/postgres/postgres-query-compiler.ts
@@ -1,15 +1,1 @@
-import { DefaultQueryCompiler } from "../deps/kysely.ts";
-
-export class PostgresQueryCompiler extends DefaultQueryCompiler {
-  protected getCurrentParameterPlaceholder(): string {
-    return "$" + this.numParameters;
-  }
-
-  protected override getLeftIdentifierWrapper(): string {
-    return '"';
-  }
-
-  protected override getRightIdentifierWrapper(): string {
-    return '"';
-  }
-}
+export { PostgresQueryCompiler } from "../deps/kysely.ts";
