83ad29ea9ac62970ab3c32cdfcc7d1e989ff88e8
Lorentz Lasson <lasson.lorentz@gmail.com>
2023-03-19 21:50:11 +0100

    Stop using deprecated field

--- src/postgres/postgres-driver.ts
+++ src/postgres/postgres-driver.ts
@@ -104,7 +104,7 @@ class PostgresConnection implements DatabaseConnection {
 
       if (result.command === "UPDATE" || result.command === "DELETE") {
         return {
-          numUpdatedOrDeletedRows: BigInt(result.rowCount!),
+          numAffectedRows: BigInt(result.rowCount!),
           rows: result.rows ?? [],
         };
       }
